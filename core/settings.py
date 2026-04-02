2e029192d288ca60b010cfbb220245fb041d88e5
Shubham <shubhamsharma68653@gmail.com>
2026-04-02 14:13:47 +0530

    User Creation Dome with Acess Token

--- core/settings.py
+++ core/settings.py
@@ -98,6 +98,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.User'
+
 # ====================== REST FRAMEWORK SETTINGS ======================
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
